606fccd326c362ae6e795cb7c8afd06e8d824edb
bekturinho <ismanalievbektur4@gmail.com>
2024-04-15 18:48:55 -0500

    Initial Commit

---
+++ FinanceProgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceProgram
+//
+//  Created by fortune cookie on 4/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
